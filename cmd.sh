74ac907a4578aeee04ffeaef44dab730568c6f6e
chendelin1982 <27513732@qq.com>
2022-01-21 19:01:37 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,8 @@
-# replace DB connection string
+#!/bin/bash
 
-# install automaticlly
+# CMD from ccr.ccs.tencentyun.com/discuzq/dzq:latest
+/usr/sbin/supervisord -c /etc/supervisord.conf
+
+# Replace DB connetion
+
+# Install wizard
