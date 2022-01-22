69f779c6ad24588f999abd6e4a87e887a7135e87
qiaofeng1227 <76487013@qq.com>
2022-01-22 16:03:41 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -6,7 +6,7 @@ sed -i "s/forumTitle.*/forumTitle\" value=\"$DISCUZQ_SITENAME\">/g" /var/www/dis
 sed -i "s/mysqlHost.*/mysqlHost\" value=\"$DISCUZQ_MYSQL_HOST\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/mysqlDatabase.*/mysqlDatabase\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/mysqlUsername.*/mysqlUsername\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" /var/www/discuz/resources/views/install/install.blade.php
-sed -i "s/password.*/password\" value=\"$DISCUZQ_MYSQL_PASSWORD\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/mysqlPassword.*/mysqlPassword\" value=\"$DISCUZQ_MYSQL_PASSWORD\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/tablePrefix.*/tablePrefix\" value=\"discuzq\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/adminUsername.*/adminUsername\" value=\"$DISCUZQ_USER\">/g" /var/www/discuz/resources/views/install/install.blade.php
 
