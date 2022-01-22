28404f9e8de0eacb0757d315a26cda9ab7b5824b
qiaofeng1227 <76487013@qq.com>
2022-01-22 12:04:18 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,8 +2,13 @@
 
 
 # Replace DB connetion
-
-echo $DISCUZQ_PASSWORD > /tmp/test.log
+sed -i "s/forumTitle.*/forumTitle\" value=\"$DISCUZQ_SITENAME\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/mysqlHost.*/mysqlHost\" value=\"$DISCUZQ_MYSQL_HOST\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/mysqlDatabase.*/mysqlDatabase\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/mysqlUsername.*/mysqlUsername\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/password.*/password\" value=\"$DISCUZQ_MYSQL_PASSWORD\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/tablePrefix.*/tablePrefix\" value=\"discuzq\">/g" /var/www/discuz/resources/views/install/install.blade.php
+sed -i "s/adminUsername.*/adminUsername\" value=\"$DISCUZQ_USER\">/g" /var/www/discuz/resources/views/install/install.blade.php
 
 # CMD from ccr.ccs.tencentyun.com/discuzq/dzq:latest
 /usr/sbin/supervisord -c /etc/supervisord.conf
