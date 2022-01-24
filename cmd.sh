f42e83fc87a8646e5a90676b19b3d236a5c6e08c
chendelin1982 <27513732@qq.com>
2022-01-24 09:39:55 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -7,7 +7,6 @@ sed -i "s/mysqlDatabase.*/mysqlDatabase\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" 
 sed -i "s/mysqlUsername.*/mysqlUsername\" value=\"$DISCUZQ_MYSQL_DATABASE\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/mysqlPassword.*/mysqlPassword\" value=\"$DISCUZQ_MYSQL_PASSWORD\">/g" /var/www/discuz/resources/views/install/install.blade.php
 sed -i "s/tablePrefix.*/tablePrefix\" value=\"discuzq\">/g" /var/www/discuz/resources/views/install/install.blade.php
-sed -i "s/adminUsername.*/adminUsername\" value=\"$DISCUZQ_USER\">/g" /var/www/discuz/resources/views/install/install.blade.php
 
 # CMD from ccr.ccs.tencentyun.com/discuzq/dzq:latest
 /usr/sbin/supervisord -c /etc/supervisord.conf
